ac7f1d646d60c5e96927c98445fc8641ddfc5970
Pritanshu69 <samantaprit1924@gmail.com>
2024-04-10 03:17:07 +0530

    .

--- src/second.rs
+++ src/second.rs
@@ -1,4 +1,6 @@
 fn main(){
-    let x = 5;
+    let mut x = 5; //mut = mutable which means I can change the value of x in next lines
     println!("The value of x is: {x}"); // This will print "The value of x is
+    let x = 69;
+    println!("The value of x is: {x}");
 }
