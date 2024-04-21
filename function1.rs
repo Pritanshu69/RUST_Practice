dfb2c234ea52af9835593f0caabfa34204967668
Pritanshu69 <samantaprit1924@gmail.com>
2024-04-21 11:49:41 +0530

    .

--- function1.rs
+++ function1.rs
@@ -1,9 +1,18 @@
-fn main(){
-    println!("Hello World!");
+// fn main(){
+//     println!("Hello World!");
 
-    another_func();
-}
+//     another_func();
+// }
 
-fn another_func() {
-    println!(", How are you?");
+// fn another_func() {
+//     println!(", How are you?");
+// }
+
+fn main() {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+
+    println!("The value of y is: {y}");
 }
